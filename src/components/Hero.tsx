3cf810283770b57195b96fbba31dd634f6cb134d
unknown <matemedranda15@gmail.com>
2025-12-02 08:40:20 -0500

    V1.2.1 Changing svg hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -89,7 +89,7 @@ const Hero = ({
       <div className="absolute bottom-32 right-0 w-48 h-48 bg-gradient-to-tl from-violet-500/15 to-transparent blur-2xl" />
 
       {/* Content */}
-      <div className="relative z-10 text-center max-w-5xl mx-auto mt-[100px] mb-[60px]">
+      <div className="relative z-10 text-center max-w-5xl mx-auto mt-[100px] mb-[100px]">
         <div className="mb-8 animate-float relative">
           {/* Logo glow effect */}
           <div className="absolute inset-0 w-48 h-48 mx-auto bg-primary/30 rounded-full blur-2xl animate-pulse-glow" />
@@ -127,13 +127,6 @@ const Hero = ({
         </div>
       </div>
 
-      {/* Curved border bottom */}
-      <div className="absolute bottom-0 left-0 right-0 overflow-hidden leading-none rotate-180 text-white">
-        <svg className="relative block w-full h-16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-          <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-            className="fill-white"></path>
-        </svg>
-      </div>
     </section>
   );
 };
